794ef96045d8e25ebb03ca8edfbf3796ae0bc7bc
MauroDiaaz <diazmauro3863@gmail.com>
2024-08-19 23:53:32 -0300

    FrontEnd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginComponent, RegistroComponent, HomeComponent, DashboardComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "home", pathMatch: "full"},
+    { path: "login", component: LoginComponent},
+    { path: "registro", component: RegistroComponent},
+    { path: "home", component: HomeComponent},
+    { path: "dashboard", component: DashboardComponent},
+    { path: "**", redirectTo: "home", pathMatch: "full"}
+];
+
+@NgModule(
+    {
+        imports: [RouterModule.forRoot(routes)],
+        exports: [RouterModule]
+    }
+)
+export class AppRoutingModule{}
